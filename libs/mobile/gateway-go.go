4fc329231ffb1c63555600a473a24528fd4d988f
Yu Fang <newfarry@126.com>
2025-07-08 16:33:10 +0800

    修改服务id组合uuid

--- libs/mobile/gateway-go.go
+++ libs/mobile/gateway-go.go
@@ -1,8 +1,10 @@
 package mobile
 
 import (
+	"fmt"
 	"github.com/OpenIoTHub/gateway-go/v2/client"
 	"github.com/OpenIoTHub/gateway-go/v2/register"
+	uuid "github.com/satori/go.uuid"
 	"log"
 	"net"
 )
@@ -19,7 +21,7 @@ func runGatewayGO() {
 		"local",
 		"localhost",
 		34323,
-		[]string{"name=gateway-go", "home-page=https://github.com/OpenIoTHub/gateway-go"},
+		[]string{"name=gateway-go", fmt.Sprintf("id=gateway-go@%s", uuid.Must(uuid.NewV4()).String()), "home-page=https://github.com/OpenIoTHub/gateway-go"},
 		0,
 		[]net.IP{net.ParseIP("127.0.0.1")},
 		[]net.IP{},
@@ -29,7 +31,7 @@ func runGatewayGO() {
 		"local",
 		"localhost",
 		5244,
-		[]string{"name=OpenlistApp", "home-page=https://github.com/OpenlistApp/OpenlistApp"},
+		[]string{"name=OpenlistApp", fmt.Sprintf("id=alist@%s", uuid.Must(uuid.NewV4()).String()), "home-page=https://github.com/OpenlistApp/OpenlistApp"},
 		0,
 		[]net.IP{net.ParseIP("127.0.0.1")},
 		[]net.IP{},
@@ -39,7 +41,7 @@ func runGatewayGO() {
 		"local",
 		"localhost",
 		9876,
-		[]string{"name=ddns-go", "home-page=https://github.com/OpenIoTHub/ddns-go"},
+		[]string{"name=ddns-go", fmt.Sprintf("id=ddns-go@%s", uuid.Must(uuid.NewV4()).String()), "home-page=https://github.com/OpenIoTHub/ddns-go"},
 		0,
 		[]net.IP{net.ParseIP("127.0.0.1")},
 		[]net.IP{},
